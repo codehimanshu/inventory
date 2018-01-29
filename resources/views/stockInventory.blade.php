6c6a8af29c49f5d7c8a58516f043a44508b430c8
Himanshu Agrawal <himanshuagrawal1998@gmail.com>
2018-01-29 23:54:34 +0530

    create add row button

--- resources/views/stockInventory.blade.php
+++ resources/views/stockInventory.blade.php
@@ -1,10 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
+<script type="text/javascript">
+    function addfield() {
+        $("#table").append('\
+                        <tr>\
+                            <td>\
+                                <input type="number" class="form-control" name="sno[]">\
+                            </td>\
+                            <td>\
+                                <select class="form-control" name="category[]">\
+                                    @foreach($categories as $category)\
+                                        <option>{{$category->category}}</option>\
+                                    @endforeach\
+                                </select>\
+                            </td>\
+                            <td>\
+                                <select class="form-control" name="tosite[]">\
+                                    <option>Site 1</option>\
+                                    <option>Site 2</option>\
+                                </select>\
+                            </td>\
+                            <td>\
+                                <input type="number" class="form-control" name="costing[]" step="0.01">\
+                            </td>\
+                            <td>\
+                                <input type="number" class="form-control" name="quantity[]" step="0">\
+                            </td>\
+                            <td>\
+                                <input type="date" class="form-control" name="date[]">\
+                            </td>\
+                        </tr>\
+            ')
+        return;   
+    }    
+</script>
+
 <div class="container">
     <div class="row">
         <center><h2>Stock</h2></center>
-        <form>
+        <form method="">
             <div class="col-md-12">
                 <table class="table">
                     <thead>
@@ -17,7 +52,7 @@
                             <th>Dated</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody id="table">
                         <tr>
                             <td>
                                 <input type="number" class="form-control" name="sno[]">
@@ -47,7 +82,7 @@
                         </tr>
                     </tbody>
                 </table>
-                <span style="float: right;"><button class="btn btn-sm btn-primary">Add Row</button></span>
+                <span style="float: right;"><input type="button" class="btn btn-sm btn-primary" onclick="addfield()" value="Add Row"></span>
                 <br><br>
                 <center><input type="submit" class="form-control" style="width: 100px;" name="submit"></center>
             </div>        
