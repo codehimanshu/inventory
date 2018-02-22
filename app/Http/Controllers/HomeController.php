9729008f4e89d869977f2dfff3ce7fd6b163e0c6
Himanshu Agrawal <himanshuagrawal1998@gmail.com>
2018-02-22 20:21:23 +0530

    add title

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,12 +28,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $title = "dashboard";
+        return view('home','title');
     }
 
     public function stockInventory() {
         $categories = SubCategory::get();
-        return view('stockInventory', compact('categories'));
+        $title = "inventory";
+        return view('stockInventory', compact('categories','title'));
     }
 
     public function saveStockInventory(Request $request) {
@@ -73,12 +75,14 @@ class HomeController extends Controller
             return redirect('/home');
         $stocks = Stock::with('subcategory')->get();
         $total_amt = Stock::sum('stock_amt');
-        return view('stockReport', compact('stocks','total_amt'));
+        $title = "report";
+        return view('stockReport', compact('stocks','total_amt','title'));
     }
 
     public function tosite() {
         $categories = SubCategory::get();
-        return view('tosite', compact('categories'));
+        $title = "inventory";
+        return view('tosite', compact('categories','title'));
     }
 
     public function saveToSite(Request $request) {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,11 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-                            <li><a href="{{ route('home') }}">Dashboard</a></li>
+                            @if($title == "dashboard")
+                                <li><a href="{{ route('home') }}">Dashboard</a></li>
+                            @else
+                                <li><a href="{{ route('home') }}">Dashboard</a></li>
+                            @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     Inventory <span class="caret"></span>
